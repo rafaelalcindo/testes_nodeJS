f57eb93d0a218d7818531648ed7292cf8e895e61
rafaelalcindo <rafael_alcindo@hotmail.com>
2017-09-18 16:16:15 -0300

    mudancas

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 var app = require('./config/server');
 
-var rotaNoticias = require('./app/routes/noticias');
-rotaNoticias(app);
+//var rotaNoticias = require('./app/routes/noticias');
+///rotaNoticias(app);
 
-var rotaHome = require('./app/routes/home');
-rotaHome(app);
+//var rotaHome = require('./app/routes/home');
+//rotaHome(app);
 
 /*var rotaFormulario = require('./app/routes/formulario_inclusao_noticia');
 rotaFormulario(app);  */
 // ou 
-var rotaFormulario = require('./app/routes/formulario_inclusao_noticia')(app);
+
+
+//var rotaFormulario = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
     console.log("Servidor ON");    
